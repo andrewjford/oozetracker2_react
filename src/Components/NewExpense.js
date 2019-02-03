63cbc91dfb0769653fe8a5fc58835e346a91b2d6
AJ <ajoeford@gmail.com>
2019-02-03 10:47:04 -0800

    update new expense design

--- src/Components/NewExpense.js
+++ src/Components/NewExpense.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { Paper, TextField, Typography, Select, InputLabel, MenuItem, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import { Redirect } from 'react-router-dom';
 
 import BackendCallout from './BackendCallout';
 
-export default class NewExpense extends React.Component {
+const styles = theme => ({
+  form: {
+    display: 'grid',
+    gridTemplateColumns: '30% 30% auto',
+    gridRowGap: '1rem'
+  },
+  container: {
+    padding: '2rem'
+  },
+  input: {
+    gridColumn: '1 / 2'
+  },
+  categoryGroup: {
+    gridColumn: '1 / 2',
+    display: 'grid',
+    gridTemplateColumns: '30% 70%'
+  },
+  categoryChild: {
+    
+  },
+  button: {
+    gridColumn: '1 / 2',
+    justifySelf: 'center'
+  }
+});
 
+class NewExpense extends React.Component {
   constructor(props) {
     super(props);
     let theDate = new Date();
@@ -78,29 +104,34 @@ export default class NewExpense extends React.Component {
   }
 
   render() {
-    
     return (
-      <Paper>
+      <Paper className={this.props.classes.container}>
         {this.redirect()}
         <Typography variant="h5" component="h3">
           New Expense
         </Typography>
-        <form onSubmit={this.handleSubmit}>
-          <TextField type="text" value={this.state.form.description} onChange={this.handleDescriptionChange}
+        <form onSubmit={this.handleSubmit} className={this.props.classes.form}>
+          <TextField type="text" value={this.state.form.description}  className={this.props.classes.input}
+                     onChange={this.handleDescriptionChange}
                      label="Description"/>
           
           <TextField type="number" value={this.state.form.amount} onChange={this.handleAmountChange}
-                     label="Amount"/>
+                     label="Amount" className={this.props.classes.input}/>
 
-          <InputLabel>Category</InputLabel>
-            <Select value={this.state.form.category} onChange={this.handleCategoryChange}>
+          <div className={this.props.classes.categoryGroup}>
+            <InputLabel className={this.props.classes.categoryChild}>Category</InputLabel>
+            <Select value={this.state.form.category} onChange={this.handleCategoryChange} className={this.props.classes.categoryChild}>
               <MenuItem value=""><em>None</em></MenuItem>
               {this.categoriesList()}
             </Select>
+          </div>
 
-          <Button type="submit" variant="contained" color="secondary" onClick={this.handleSubmit}>Submit</Button>
+          <Button type="submit" variant="contained" color="secondary" 
+                  onClick={this.handleSubmit} className={this.props.classes.button}>Submit</Button>
         </form>
       </Paper>
     )
   }
 }
+
+export default withStyles(styles)(NewExpense);
