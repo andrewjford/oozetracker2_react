5b0a7001bb17008e100494ac37e7f6c754f79805
AJ <ajoeford@gmail.com>
2019-05-05 12:22:13 -0700

    update styling of error box

--- src/components/ErrorDisplay.js
+++ src/components/ErrorDisplay.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import ErrorIcon from '@material-ui/icons/ErrorOutline';
 
 const ErrorDisplay = (props) => {
   const lines = props.errors.map((error) => {
-    return (<Typography key={error} variant="body1" color="error">{error}</Typography>);
+    return (
+      <div className={props.classes.row} key={error}>
+        <ErrorIcon className={props.classes.icon}/>
+        <Typography variant="body1" color="error" className={props.classes.errorText}>
+          {error}
+        </Typography>
+      </div>
+    );
   });
 
-  return (
-    <div>
-      {lines}
-    </div>
-  );
+  if (props.errors.length > 0) {
+    return (
+      <div className={props.classes.errorBox}>
+        {lines}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
-export default ErrorDisplay;
+const styles = theme => ({
+  errorBox: {
+    backgroundColor: "#f3f3f3",
+    padding: "0.5rem",
+  },
+  row: {
+    display: "grid",
+    gridTemplateColumns: "5% 95%",
+    gridColumnGap: "0.5rem",
+  },
+  errorText: {
+    alignSelf: "center",
+    justifySelf: "start",
+    gridColumnStart: 2,
+    gridColumnEnd: 2,
+  },
+  icon: {
+    alignSelf: "center",
+    gridColumnStart: 1,
+    gridColumnEnd: 2,
+    color: "red",
+  }
+});
+
+export default withStyles(styles)(ErrorDisplay);
