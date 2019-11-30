b1e092d3e0a865197cee709ccd41e6d143bc145e
AJ <ajoeford@gmail.com>
2019-11-30 14:17:29 -0800

    fix width on login/register xs screen

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -1,84 +1,84 @@
-import React from 'react';
-import { Paper, TextField, Typography, Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { Redirect, withRouter } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import ErrorDisplay from '../ErrorDisplay';
-import Loading from '../Loading';
+import React from "react";
+import { Paper, TextField, Typography, Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { Redirect, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import ErrorDisplay from "../ErrorDisplay";
+import Loading from "../Loading";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       form: {
-        email: '',
-        password: '',
+        email: "",
+        password: ""
       },
       history: props.history,
       errors: [],
-      loading: false,
+      loading: false
     };
   }
 
-  convertDateToString = (date) => {
+  convertDateToString = date => {
     let month = date.getMonth() + 1;
     month = month.toString().length === 1 ? "0" + month : month;
     let day = date.getDate();
     day = day.toString().length === 1 ? "0" + day : day;
     return `${date.getFullYear()}-${month}-${day}`;
-  }
+  };
 
-  login = (input) => {
-    this.setState({loading: true});
-    this.props.login(input)
+  login = input => {
+    this.setState({ loading: true });
+    this.props
+      .login(input)
       .then(() => {
         this.setState({
           loading: false,
-          redirect: "/",
+          redirect: "/"
         });
       })
       .catch(error => {
         const parsedError = JSON.parse(error.message);
         if (!parsedError) {
-          this.setState({errors: [error.message]});
+          this.setState({ errors: [error.message] });
         }
         this.setState({
-          errors: parsedError.constructor === Array ? parsedError : [parsedError],
-          loading: false,
+          errors:
+            parsedError.constructor === Array ? parsedError : [parsedError],
+          loading: false
         });
       });
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
     this.login(this.state.form);
-  }
+  };
 
-  handleEmailChange = (event) => {
+  handleEmailChange = event => {
     this.setState({
       form: {
         ...this.state.form,
         email: event.target.value
       }
     });
-  }
+  };
 
-  handlePasswordChange = (event) => {
+  handlePasswordChange = event => {
     this.setState({
       form: {
         ...this.state.form,
         password: event.target.value
       }
     });
-  }
+  };
 
   loadingSpinner = () => {
     if (this.state.loading) {
-      return (
-        <Loading />
-      )
+      return <Loading />;
     }
-  }
+  };
 
   loginForm = () => {
     if (this.state.loading) {
@@ -86,22 +86,39 @@ class Login extends React.Component {
     } else {
       return (
         <form onSubmit={this.handleSubmit} className={this.props.classes.form}>
-          <TextField type="text" value={this.state.form.email}  className={this.props.classes.input}
-                      onChange={this.handleEmailChange}
-                      label="Email"/>
-          
-          <TextField type="password" value={this.state.form.password} onChange={this.handlePasswordChange}
-                      label="Password" className={this.props.classes.input}/>
+          <TextField
+            type="text"
+            value={this.state.form.email}
+            className={this.props.classes.input}
+            onChange={this.handleEmailChange}
+            label="Email"
+          />
+
+          <TextField
+            type="password"
+            value={this.state.form.password}
+            onChange={this.handlePasswordChange}
+            label="Password"
+            className={this.props.classes.input}
+          />
 
           <div className={this.props.classes.buttons}>
-            <Button type="submit" variant="contained" color="secondary" 
-                    onClick={this.handleSubmit} className={this.props.classes.button}>Login</Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              onClick={this.handleSubmit}
+              className={this.props.classes.button}
+            >
+              Login
+            </Button>
           </div>
-          <Typography variant="body2" className={this.props.classes.justifyCenter}>
+          <Typography
+            variant="body2"
+            className={this.props.classes.justifyCenter}
+          >
             <span>Not a member? Register </span>
-            <Link to="/register">
-              here
-            </Link>
+            <Link to="/register">here</Link>
           </Typography>
         </form>
       );
@@ -110,11 +127,13 @@ class Login extends React.Component {
 
   render() {
     if (this.props.isLoggedIn || this.state.redirect) {
-      return <Redirect to={'/'} />;
+      return <Redirect to={"/"} />;
     } else {
       return (
         <Paper className={this.props.classes.paper}>
-          <Typography variant="h5" component="h3">Login</Typography>
+          <Typography variant="h5" component="h3">
+            Login
+          </Typography>
           <ErrorDisplay errors={this.state.errors} />
           {this.loginForm()}
         </Paper>
@@ -125,40 +144,37 @@ class Login extends React.Component {
 
 const styles = theme => ({
   form: {
-    display: 'grid',
-    gridRowGap: '1rem',
-    paddingTop: '1rem',
+    display: "grid",
+    gridRowGap: "1rem",
+    paddingTop: "1rem"
   },
   paper: {
     gridColumnStart: 3,
     gridColumnEnd: 5,
-    padding: '2rem',
-    margin: '0 7rem',
-    minHeight: '15rem',
-    [theme.breakpoints.down('sm')]: {
-      gridColumn: "2 / 6",
+    padding: "2rem",
+    minHeight: "15rem",
+    [theme.breakpoints.down("sm")]: {
+      gridColumn: "2 / 6"
     },
-    [theme.breakpoints.down('xs')]: {
-      gridColumn: "1 / -1",
-    },
+    [theme.breakpoints.down("xs")]: {
+      gridColumn: "1 / -1"
+    }
   },
   categoryGroup: {
-    gridColumn: '1 / 2',
-    display: 'grid',
-    gridTemplateColumns: '30% 70%'
+    gridColumn: "1 / 2",
+    display: "grid",
+    gridTemplateColumns: "30% 70%"
   },
-  categoryChild: {
-    
-  },
+  categoryChild: {},
   buttons: {
-    gridColumn: '1 / 2',
-    justifySelf: 'center',
+    gridColumn: "1 / 2",
+    justifySelf: "center"
   },
   button: {
-    margin: '0 0.5rem'
+    margin: "0 0.5rem"
   },
   justifyCenter: {
-    justifySelf: 'center',
+    justifySelf: "center"
   }
 });
 
--- src/components/Account/Register.js
+++ src/components/Account/Register.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import { Paper, TextField, Typography, Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { Redirect, withRouter } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import ErrorDisplay from '../ErrorDisplay';
+import React from "react";
+import { Paper, TextField, Typography, Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { Redirect, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import ErrorDisplay from "../ErrorDisplay";
 
 const styles = theme => ({
   form: {
-    display: 'grid',
-    gridRowGap: '1rem',
-    paddingTop: '1rem',
+    display: "grid",
+    gridRowGap: "1rem",
+    paddingTop: "1rem"
   },
   paper: {
     gridColumnStart: 3,
     gridColumnEnd: 5,
-    padding: '2rem',
-    margin: '0 7rem',
+    padding: "2rem",
+    [theme.breakpoints.down("xs")]: {
+      gridColumn: "1 / -1"
+    }
   },
   categoryGroup: {
-    gridColumn: '1 / 2',
-    display: 'grid',
-    gridTemplateColumns: '30% 70%'
+    gridColumn: "1 / 2",
+    display: "grid",
+    gridTemplateColumns: "30% 70%"
   },
   hidden: {
-    visibility: 'hidden',
+    visibility: "hidden"
   },
   buttons: {
-    gridColumn: '1 / 2',
-    justifySelf: 'center',
+    gridColumn: "1 / 2",
+    justifySelf: "center"
   },
   button: {
-    margin: '0 0.5rem'
+    margin: "0 0.5rem"
   },
   justifyCenter: {
-    justifySelf: 'center',
+    justifySelf: "center"
   }
 });
 
@@ -42,130 +44,155 @@ class Register extends React.Component {
     super(props);
     this.state = {
       form: {
-        name: '',
-        email: '',
-        password: '',
+        name: "",
+        email: "",
+        password: ""
       },
       history: props.history,
-      errors: [],
+      errors: []
     };
   }
 
-  convertDateToString = (date) => {
+  convertDateToString = date => {
     let month = date.getMonth() + 1;
     month = month.toString().length === 1 ? "0" + month : month;
     let day = date.getDate();
     day = day.toString().length === 1 ? "0" + day : day;
     return `${date.getFullYear()}-${month}-${day}`;
-  }
+  };
 
-  register = (input) => {
-    this.props.register(input)
+  register = input => {
+    this.props
+      .register(input)
       .then(() => {
-        this.setState({redirect: "/pleaseverify"});
+        this.setState({ redirect: "/pleaseverify" });
       })
       .catch(error => {
         const parsedError = JSON.parse(error.message);
         if (!parsedError) {
-          this.setState({errors: [error.message]});
+          this.setState({ errors: [error.message] });
         }
         this.setState({
-          errors: parsedError.constructor === Array ? parsedError : [parsedError]
+          errors:
+            parsedError.constructor === Array ? parsedError : [parsedError]
         });
       });
-  }
+  };
 
   redirect = () => {
-    const {redirect} = this.state;
+    const { redirect } = this.state;
     if (!!redirect) {
-      return (<Redirect to={redirect}/>)
+      return <Redirect to={redirect} />;
     }
-  }
+  };
 
-  showError = (errorMessage) => {
-    this.setState({errors: [errorMessage]});
-  }
+  showError = errorMessage => {
+    this.setState({ errors: [errorMessage] });
+  };
 
-  validateInput = (form) => {
+  validateInput = form => {
     if (form.name === "" || form.email === "" || form.pasword === "") {
-      this.showError('Please complete all fields.');
+      this.showError("Please complete all fields.");
       return false;
     } else {
       return true;
     }
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
     const formIsComplete = this.validateInput(this.state.form);
     if (formIsComplete) {
       this.register(this.state.form);
     }
-  }
+  };
 
-  handleCancel = (event) => {
+  handleCancel = event => {
     this.state.history.goBack();
-  }
+  };
 
-  handleEmailChange = (event) => {
+  handleEmailChange = event => {
     this.setState({
       form: {
         ...this.state.form,
         email: event.target.value
       }
     });
-  }
+  };
 
-  handlePasswordChange = (event) => {
+  handlePasswordChange = event => {
     this.setState({
       form: {
         ...this.state.form,
         password: event.target.value
       }
     });
-  }
+  };
 
-  handleNameChange = (event) => {
+  handleNameChange = event => {
     this.setState({
       form: {
         ...this.state.form,
-        name: event.target.value,
+        name: event.target.value
       }
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <Paper className={this.props.classes.paper}>
         {this.redirect()}
-        <Typography variant="h5" component="h3">Register</Typography>
-        <ErrorDisplay errors={this.state.errors}/>
+        <Typography variant="h5" component="h3">
+          Register
+        </Typography>
+        <ErrorDisplay errors={this.state.errors} />
 
         <form onSubmit={this.handleSubmit} className={this.props.classes.form}>
-          <TextField type="text" value={this.state.form.name} className={this.props.classes.input}
-                      onChange={this.handleNameChange}
-                      label="Name"/>
+          <TextField
+            type="text"
+            value={this.state.form.name}
+            className={this.props.classes.input}
+            onChange={this.handleNameChange}
+            label="Name"
+          />
 
-          <TextField type="text" value={this.state.form.email}  className={this.props.classes.input}
-                     onChange={this.handleEmailChange}
-                     label="Email"/>
-          
-          <TextField type="password" value={this.state.form.password} onChange={this.handlePasswordChange}
-                     label="Password" className={this.props.classes.input}/>
+          <TextField
+            type="text"
+            value={this.state.form.email}
+            className={this.props.classes.input}
+            onChange={this.handleEmailChange}
+            label="Email"
+          />
+
+          <TextField
+            type="password"
+            value={this.state.form.password}
+            onChange={this.handlePasswordChange}
+            label="Password"
+            className={this.props.classes.input}
+          />
 
           <div className={this.props.classes.buttons}>
-            <Button type="submit" variant="contained" color="secondary" 
-                    onClick={this.handleSubmit} className={this.props.classes.button}>Register</Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              onClick={this.handleSubmit}
+              className={this.props.classes.button}
+            >
+              Register
+            </Button>
           </div>
-          <Typography variant="body2" className={this.props.classes.justifyCenter}>
+          <Typography
+            variant="body2"
+            className={this.props.classes.justifyCenter}
+          >
             <span>Already a member? Login </span>
-            <Link to="/login">
-              here
-            </Link>
+            <Link to="/login">here</Link>
           </Typography>
         </form>
       </Paper>
-    )
+    );
   }
 }
 
