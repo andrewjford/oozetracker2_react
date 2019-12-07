71f34a30343f270fcb801508a436d19f15af4856
AJ <ajoeford@gmail.com>
2019-12-07 11:32:06 -0800

    update amount input on expense form

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,45 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Paper, TextField, Typography, Select, InputLabel, MenuItem, Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { Redirect, withRouter } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+  Paper,
+  TextField,
+  Typography,
+  Select,
+  InputLabel,
+  MenuItem,
+  Button,
+  Input,
+  InputAdornment,
+  FormControl
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { Redirect, withRouter } from "react-router-dom";
 
-import { createExpense, updateExpense } from '../../actions/expenseActions';
-
-const styles = theme => ({
-  form: {
-    display: 'grid',
-    gridRowGap: '1rem',
-    paddingTop: '1rem'
-  },
-  paper: {
-    gridColumnStart: 2,
-    gridColumnEnd: 4,
-    padding: '2rem',
-    [theme.breakpoints.down('sm')]: {
-      gridColumn: "2 / 5",
-    },
-    [theme.breakpoints.down('xs')]: {
-      gridColumn: "1 / -1",
-    },
-  },
-  categoryGroup: {
-    gridColumn: '1 / 2',
-    display: 'grid',
-    gridTemplateColumns: '30% 70%'
-  },
-  categoryChild: {
-    
-  },
-  buttons: {
-    gridColumn: '1 / 2',
-    justifySelf: 'center',
-  },
-  button: {
-    margin: '0 0.5rem'
-  }
-});
+import { createExpense, updateExpense } from "../../actions/expenseActions";
 
 class ExpenseForm extends React.Component {
   constructor(props) {
@@ -52,101 +29,88 @@ class ExpenseForm extends React.Component {
           category: props.expense.category_id,
           date: this.convertDateToString(new Date(props.expense.date))
         },
-        history: props.history,
-      }
+        history: props.history
+      };
     } else {
       this.state = {
         mode: "new",
         form: {
-          description: '',
+          description: "",
           amount: 0,
           date: this.convertDateToString(new Date()),
-          category: 'choose one',
+          category: "choose one"
         },
-        history: props.history,
+        history: props.history
       };
     }
   }
 
-  convertDateToString = (date) => {
+  convertDateToString = date => {
     let month = date.getMonth() + 1;
     month = month.toString().length === 1 ? "0" + month : month;
     let day = date.getDate();
     day = day.toString().length === 1 ? "0" + day : day;
     return `${date.getFullYear()}-${month}-${day}`;
-  }
+  };
 
   categoriesList = () => {
     return this.props.categories.map((category, index) => {
-      return (<MenuItem key={index} value={category.id}>{category.name}</MenuItem>);
+      return (
+        <MenuItem key={index} value={category.id}>
+          {category.name}
+        </MenuItem>
+      );
     });
-  }
+  };
 
-  createExpense = (newExpense) => {
+  createExpense = newExpense => {
     this.props.createExpense(newExpense);
-    this.setState({redirect: "/"});
-  }
+    this.setState({ redirect: "/" });
+  };
 
-  updateExpense = (expense) => {
+  updateExpense = expense => {
     this.props.updateExpense(expense);
-    this.setState({redirect: `/expenses/${expense.id}`})
-  }
+    this.setState({ redirect: `/expenses/${expense.id}` });
+  };
 
   redirect = () => {
-    const {redirect} = this.state;
+    const { redirect } = this.state;
     if (!!redirect) {
-      return (<Redirect to={redirect}/>)
+      return <Redirect to={redirect} />;
     }
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
     if (this.state.mode === "edit") {
       this.updateExpense(this.state.form);
     } else {
       this.createExpense(this.state.form);
     }
-  }
+  };
 
-  handleCancel = (event) => {
+  handleCancel = event => {
     this.state.history.goBack();
-  }
+  };
 
-  handleDescriptionChange = (event) => {
+  handleChange = event => {
+    const field = event.target.id;
     this.setState({
       form: {
         ...this.state.form,
-        description: event.target.value
+        [field]: event.target.value
       }
     });
-  }
+  };
 
-  handleAmountChange = (event) => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        amount: event.target.value
-      }
-    });
-  }
-
-  handleCategoryChange = (event) => {
+  handleCategoryChange = event => {
     this.setState({
       form: {
         ...this.state.form,
         category: event.target.value
       }
     });
-  }
-
-  handleDateChange = (event) => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        date: event.target.value
-      }
-    });
-  }
+  };
 
   render() {
     const header = () => {
@@ -156,46 +120,131 @@ class ExpenseForm extends React.Component {
           {title}
         </Typography>
       );
-    }
+    };
 
     return (
       <Paper className={this.props.classes.paper}>
         {this.redirect()}
         {header()}
         <form onSubmit={this.handleSubmit} className={this.props.classes.form}>
-          <TextField id="wt" value={this.state.form.date} type="date" label="Date" onChange={this.handleDateChange}/>
+          <TextField
+            id="date"
+            value={this.state.form.date}
+            type="date"
+            label="Date"
+            onChange={this.handleChange}
+          />
 
-          <TextField type="text" value={this.state.form.description}  className={this.props.classes.input}
-                     onChange={this.handleDescriptionChange}
-                     label="Description"/>
-          
-          <TextField type="number" value={this.state.form.amount} onChange={this.handleAmountChange}
-                     label="Amount" className={this.props.classes.input}/>
+          <TextField
+            id="description"
+            type="text"
+            value={this.state.form.description}
+            className={this.props.classes.input}
+            onChange={this.handleChange}
+            label="Description"
+          />
+
+          <FormControl
+            fullWidth
+            className={this.props.classes.input}
+            variant="standard"
+          >
+            <InputLabel htmlFor="amount">Amount</InputLabel>
+            <Input
+              id="amount"
+              type="number"
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              value={this.state.form.amount}
+              onChange={this.handleChange}
+              onFocus={event => event.target.select()}
+            ></Input>
+          </FormControl>
 
           <div className={this.props.classes.categoryGroup}>
-            <InputLabel className={this.props.classes.categoryChild}>Category</InputLabel>
-            <Select value={this.state.form.category} onChange={this.handleCategoryChange} className={this.props.classes.categoryChild}>
-              <MenuItem value=""><em>None</em></MenuItem>
+            <InputLabel className={this.props.classes.categoryChild}>
+              Category
+            </InputLabel>
+            <Select
+              value={this.state.form.category}
+              onChange={this.handleCategoryChange}
+              className={this.props.classes.categoryChild}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
               {this.categoriesList()}
             </Select>
           </div>
           <div className={this.props.classes.buttons}>
-            <Button type="submit" variant="contained" color="secondary" 
-                    onClick={this.handleSubmit} className={this.props.classes.button}>Submit</Button>
-            <Button variant="contained" color="secondary" 
-                    onClick={this.handleCancel} className={this.props.classes.button}>Cancel</Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={this.handleCancel}
+              className={this.props.classes.button}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              onClick={this.handleSubmit}
+              className={this.props.classes.button}
+            >
+              Submit
+            </Button>
           </div>
         </form>
       </Paper>
-    )
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    createExpense,
-    updateExpense,
-  }, dispatch)
-}
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      createExpense,
+      updateExpense
+    },
+    dispatch
+  );
+};
 
-export default connect(null, mapDispatchToProps)(withRouter(withStyles(styles)(ExpenseForm)));
+const styles = theme => ({
+  form: {
+    display: "grid",
+    gridRowGap: "1rem",
+    paddingTop: "1rem"
+  },
+  paper: {
+    gridColumnStart: 2,
+    gridColumnEnd: 4,
+    padding: "2rem",
+    [theme.breakpoints.down("sm")]: {
+      gridColumn: "2 / 5"
+    },
+    [theme.breakpoints.down("xs")]: {
+      gridColumn: "1 / -1"
+    }
+  },
+  categoryGroup: {
+    gridColumn: "1 / 2",
+    display: "grid",
+    gridTemplateColumns: "30% 70%"
+  },
+  categoryChild: {},
+  buttons: {
+    gridColumn: "1 / 2",
+    justifySelf: "center"
+  },
+  button: {
+    margin: "0 0.5rem"
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(withRouter(withStyles(styles)(ExpenseForm)));
